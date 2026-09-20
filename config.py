6aae0bd17ed23a3d6f656f46bc325a2e271a9937
Akki <129473687+UntoldAkki@users.noreply.github.com>
2026-09-20 12:01:54 +0530

    Update CHANNEL_ID environment variable default value

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "8272927957:AAFrMkKgB3wAmmrtoMJ-GxqMNzjcUyCKuIo"
 OWNER_ID = list(map(int, getenv("OWNER_ID", "7778185746").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://akkiraw_db_user:ca0dOXtD2ibEwQy3@cluster0.kwcj0zx.mongodb.net/?appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1003369488985")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002263750687"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002272912029"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
